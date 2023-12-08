d6e0e32b88e89975abc786ad1b9c6421d8197d31
Yasir Alakus <yasiralakus11@gmail.com>
2023-12-08 16:21:52 +0300

    desktop icin tamamen calisir hale getirildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,47 @@ import { useState } from "react"
 
 export default function App() {
 
-    const [dayControl, setDayControl] = useState(1)
-    const [monthControl, setMonthControl] = useState(1)
-    const [yearControl, setYearControl] = useState(1900)
+    const [dayControl, setDayControl] = useState(null)
+    const [monthControl, setMonthControl] = useState(null)
+    const [yearControl, setYearControl] = useState(null)
+    const [ageYears, setAgeYears] = useState('--');
+    const [ageMonths, setAgeMonths] = useState('--');
+    const [ageDays, setAgeDays] = useState('--');
 
+    function handleClick(e) {
+        e.preventDefault();
+        calculator();
+    }
 
+    function calculator() {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth() + 1;
+        const currentDay = new Date().getDate();
+
+        let calculatedAgeYears = currentYear - parseInt(yearControl, 10);
+        let calculatedAgeMonths = currentMonth - parseInt(monthControl, 10);
+        let calculatedAgeDays = currentDay - parseInt(dayControl, 10);
+
+        if (calculatedAgeMonths < 0) {
+        calculatedAgeYears--;
+        calculatedAgeMonths += 12;
+        }
+
+        if (calculatedAgeDays < 0) {
+        const lastMonthDays = new Date(
+            currentYear,
+            currentMonth - 1,
+            0
+        ).getDate();
+        calculatedAgeMonths--;
+        calculatedAgeDays += lastMonthDays;
+        }
+
+        setAgeYears(calculatedAgeYears);
+        setAgeMonths(calculatedAgeMonths);
+        setAgeDays(calculatedAgeDays);
+
+        };
 
     return (
             <div className="box">
@@ -17,7 +53,7 @@ export default function App() {
                         <div className="formItem">
 
                             <label htmlFor="">DAY</label>
-                            <input type="number" value={dayControl} onChange={function(e) {
+                            <input placeholder="DD" type="number" value={dayControl} onChange={function(e) {
                                 let newValue = parseInt(e.target.value);
                                 if (newValue >= 1 && newValue <= 31) {
                                     setDayControl(newValue);
@@ -29,7 +65,7 @@ export default function App() {
                         <div className="formItem">
 
                             <label htmlFor="">MONTH</label>
-                            <input type="number" value={monthControl} onChange={function(e) {
+                            <input placeholder="MM" type="number" value={monthControl} onChange={function(e) {
                                 let newValue = parseInt(e.target.value);
                                 if (newValue >= 1 && newValue <= 12) {
                                     setMonthControl(newValue);
@@ -41,7 +77,7 @@ export default function App() {
                         <div className="formItem">
 
                             <label htmlFor="">YEAR</label>
-                            <input type="number" value={yearControl} onChange={function(e) {
+                            <input placeholder="YYYY" type="number" value={yearControl} onChange={function(e) {
                                 let newValue = parseInt(e.target.value);
                                 if (newValue >= 1900 && newValue <= 2023) {
                                     setYearControl(newValue);
@@ -51,10 +87,20 @@ export default function App() {
                         </div>
 
                     </div>
-                    
-                    <button><img src="./src/assets/img/button-vector.png" alt="" /></button>
+
+                    <button onClick={handleClick}><img src="./src/assets/img/button-vector.png" alt="" /></button>
 
                 </form>
+
+                <div className="results">
+                    
+                    <h1><span>{ageYears}</span> years</h1>
+                    <h1><span>{ageMonths}</span> months</h1>
+                    <h1><span>{ageDays}</span> days</h1>
+
+                      
+
+                </div>
             </div>
     )
 }
